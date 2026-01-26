b9d086bab378d6d028f2d14a3f142a1e83985c13
Valeron <valerontoscano@gmail.com>
2026-01-26 20:14:19 +0530

    fix: load local env for non prod env

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 import os
-load_dotenv()
+
+if os.getenv("ENVIRONMENT") != "production":
+    from dotenv import load_dotenv
+    load_dotenv()
+
 
 # Replace with your actual MySQL credentials
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
